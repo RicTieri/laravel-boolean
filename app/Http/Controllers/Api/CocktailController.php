cd7e7d5a560b99d7c698fa9bd94fd78d5f2f8606
RicTieri <tieri.riccardo@gmail.com>
2024-02-26 21:55:55 +0100

    added migration and seeder

--- app/Http/Controllers/Api/CocktailController.php
+++ app/Http/Controllers/Api/CocktailController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
 use Illuminate\Http\Request;
 
 class CocktailController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $cocktails = Cocktail::all();
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $cocktails
+            ],
+        );
+    }
 }
